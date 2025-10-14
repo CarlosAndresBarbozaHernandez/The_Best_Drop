fa9d6361cdf4537ee9d79023c250b8702d396066
Carlos Andres Barboza Hernandez <152022250+CarlosAndresBarbozaHernandez@users.noreply.github.com>
2025-10-13 21:30:30 -0600

    Primer commit del proyecto The Best Drop

---
+++ The_Best_Drop/src/main/java/com/main/demo/TheBestDropApplication.java
@@ -0,0 +1,13 @@
+package com.main.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheBestDropApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheBestDropApplication.class, args);
+	}
+
+}
